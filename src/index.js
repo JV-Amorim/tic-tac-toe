3a1ed070a0b180c518fa6e1ddaf76422ec4391a6
João Vitor Amorim <joaovitoramorimx@gmail.com>
2022-03-20 19:08:06 -0300

    feat: switching next player in Board component

--- src/index.js
+++ src/index.js
@@ -14,14 +14,22 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.getNextPlayerCharacter();
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  getNextPlayerCharacter() {
+    return this.state.xIsNext ? 'X' : 'O';
   }
 
   renderSquare(i) {
@@ -34,7 +42,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.getNextPlayerCharacter()}`;
 
     return (
       <div>
